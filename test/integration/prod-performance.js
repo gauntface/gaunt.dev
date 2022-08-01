3fbcbc0fc1dc811c79fb65fd20a9da19eec38157
Matt Gaunt-Seo <hello@gaunt.dev>
2022-07-31 17:08:34 -0700

    Adding eslint rules (#144)

--- test/integration/prod-performance.js
+++ test/integration/prod-performance.js
@@ -2,9 +2,9 @@ import test from 'ava';
 import {runLeaderboard} from '../utils/leaderboard.js';
 
 test('Performance Dashboard', async (t) => {
-  t.timeout(10 * 60 * 1000, 'Perf leaderboard is limited to 10mins');
+	t.timeout(10 * 60 * 1000, 'Perf leaderboard is limited to 10mins');
 
-  await runLeaderboard('https://www.gaunt.dev', 5);
+	await runLeaderboard('https://www.gaunt.dev', 5);
 
-  t.pass();
+	t.pass();
 })
--- test/runtime-errors.js
+++ test/runtime-errors.js
@@ -6,98 +6,98 @@ let addr;
 let browser;
 
 test.before(async (t) => {
-  // Server for project
-  addr = await startServer();
+	// Server for project
+	addr = await startServer();
 });
 test.before(async (t) => {
-  // Start browser
-  browser = await puppeteer.launch();
+	// Start browser
+	browser = await puppeteer.launch();
 })
 
 test.after('cleanup', async (t) => {
-  // This runs before all tests
-  stopServer();
+	// This runs before all tests
+	stopServer();
 
-  await browser.close();
+	await browser.close();
 });
 
 test.beforeEach(async (t) => {
-  // Create new page for test
-  t.context.page = await browser.newPage();
+	// Create new page for test
+	t.context.page = await browser.newPage();
 
-  // Ensure we get 200 responses from the server
-  t.context.page.on('response', (response) => {
-    if (response) {
-      t.deepEqual(response.status(), 200);
-    }
-  })
+	// Ensure we get 200 responses from the server
+	t.context.page.on('response', (response) => {
+		if (response) {
+			t.deepEqual(response.status(), 200);
+		}
+	})
 });
 
 test.afterEach(async (t) => {
-  await t.context.page.close();
+	await t.context.page.close();
 })
 
 const pages = [
-  {
-    title: 'index',
-    url: '/',
-  },
-  {
-    title: 'resume',
-    url: '/resume',
-  },
-  {
-    title: 'blog-archive',
-    url: '/blog',
-  },
-  {
-    title: 'blog-post',
-    url: '/blog/2020/gauntface.com-version-2020/',
-  },
-  {
-    title: 'projects-list',
-    url: '/projects',
-  },
-  {
-    title: 'projects-page',
-    url: '/projects/pin-it/',
-  },
+	{
+		title: 'index',
+		url: '/',
+	},
+	{
+		title: 'resume',
+		url: '/resume',
+	},
+	{
+		title: 'blog-archive',
+		url: '/blog',
+	},
+	{
+		title: 'blog-post',
+		url: '/blog/2020/gauntface.com-version-2020/',
+	},
+	{
+		title: 'projects-list',
+		url: '/projects',
+	},
+	{
+		title: 'projects-page',
+		url: '/projects/pin-it/',
+	},
 ];
 
 for (const p of pages) {
-  test(p.title, async (t) => {
-    const page = t.context.page;
+	test(p.title, async (t) => {
+		const page = t.context.page;
 
-    const failedRequests = [];
-    const consoleErrors = [];
-    // Catch all failed requests like 4xx..5xx status codes
-    page.on('requestfailed', request => {
-      failedRequests.push(request);
-    });
-    // Catch console log errors
-    page.on("pageerror", err => {
-        consoleErrors.push(err)
-    });
+		const failedRequests = [];
+		const consoleErrors = [];
+		// Catch all failed requests like 4xx..5xx status codes
+		page.on('requestfailed', request => {
+			failedRequests.push(request);
+		});
+		// Catch console log errors
+		page.on("pageerror", err => {
+			consoleErrors.push(err)
+		});
 
-    // Load webpage
-    await page.goto(`${addr}${p.url}`, {
-      waitUntil: 'networkidle0',
-    });
+		// Load webpage
+		await page.goto(`${addr}${p.url}`, {
+			waitUntil: 'networkidle0',
+		});
 
-    if (failedRequests.length > 0) {
-      console.log(`Failed network requests:`);
-      for (const fr of failedRequests) {
-        console.log(`    - url: ${fr.url()}, errText: ${fr.failure().errorText}, method: ${fr.method()}`);
-      }
-    }
-    if (consoleErrors.length > 0) {
-      console.log(`Console errors:`);
-      for (const err of consoleErrors) {
-        console.log(`    - url: ${err.toString()}`);
-      }
-    }
+		if (failedRequests.length > 0) {
+			console.log(`Failed network requests:`);
+			for (const fr of failedRequests) {
+				console.log(`    - url: ${fr.url()}, errText: ${fr.failure().errorText}, method: ${fr.method()}`);
+			}
+		}
+		if (consoleErrors.length > 0) {
+			console.log(`Console errors:`);
+			for (const err of consoleErrors) {
+				console.log(`    - url: ${err.toString()}`);
+			}
+		}
 
-    t.deepEqual(failedRequests, [], `There were ${failedRequests.length} failed network requests`)
-    t.deepEqual(consoleErrors, [], `There were ${consoleErrors.length} console errors`)
-  })
+		t.deepEqual(failedRequests, [], `There were ${failedRequests.length} failed network requests`)
+		t.deepEqual(consoleErrors, [], `There were ${consoleErrors.length} console errors`)
+	})
 }
