621073eb21bd1dd5c43adb1d918bb216ceb65cde
Matt Gaunt-Seo <hello@gaunt.dev>
2022-08-26 16:14:43 -0700

    Trying to add some better logs (#158)

--- themes/gaunt.dev/assets/js/js-theme-async.ts
+++ themes/gaunt.dev/assets/js/js-theme-async.ts
@@ -8,27 +8,9 @@ function run() {
     'light': 'u-theme--light',
   };
 
-  const theme = localStorage.getItem('theme');
-  if (theme) {
-    for (const t of Object.keys(themes)) {
-      const c = themes[t]
-      if (t == theme) {
-        document.body.classList.add(c);
-      } else {
-        document.body.classList.remove(c);
-      }
-    }
-  }
-
-  const elements = document.querySelectorAll('.js-theme');
-  for (const e of elements) {
-    e.addEventListener('click', function(evt){
-      if (!evt || !evt.target) {
-        return;
-      }
-      const ele = evt.target as HTMLElement;
-      const theme = ele.getAttribute('data-theme');
-
+  try {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
       for (const t of Object.keys(themes)) {
         const c = themes[t]
         if (t == theme) {
@@ -37,18 +19,41 @@ function run() {
           document.body.classList.remove(c);
         }
       }
+    }
 
-      if (theme) {
-        localStorage.setItem('theme', theme);
-      } else {
-        localStorage.removeItem('theme');
-      }
-    })
-  }
+    const elements = document.querySelectorAll('.js-theme');
+    for (const e of elements) {
+      e.addEventListener('click', function(evt){
+        if (!evt || !evt.target) {
+          return;
+        }
+        const ele = evt.target as HTMLElement;
+        const theme = ele.getAttribute('data-theme');
 
-  const container = document.querySelector('.js-theme-container');
-  if (container) {
-    container.classList.remove('c-footer__themes--disabled');
+        for (const t of Object.keys(themes)) {
+          const c = themes[t]
+          if (t == theme) {
+            document.body.classList.add(c);
+          } else {
+            document.body.classList.remove(c);
+          }
+        }
+
+        if (theme) {
+          localStorage.setItem('theme', theme);
+        } else {
+          localStorage.removeItem('theme');
+        }
+      })
+    }
+
+    const container = document.querySelector('.js-theme-container');
+    if (container) {
+      container.classList.remove('c-footer__themes--disabled');
+    }
+  } catch(err) {
+    console.error(`Failed to setup theme: `, err);
+    throw new Error(`Failed to setup theme: ${err}`);
   }
 }
 
